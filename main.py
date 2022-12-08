a06f8b6479f8d84e879c7bf323d2307757282ca0
Michael Spiese <spies046@umn.edu>
2022-12-08 11:21:47 -0600

    Hn started
    Heuristic distance function implemented

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+from queue import PriorityQueue
 
-# Data type needs:
-#    - Node Value (position tuple)
-#    - Connected nodes (List of point tuples, dictionary of distances?)
 class Graph:
-    def __init__(self,nodes=None):
+    def __init__(self,solution,nodes=None):
         if nodes is None:
             nodes = {}
         self.nodes = nodes
+        self.solution = solution
+
     def getVertices(self):
         return list(self.nodes.keys())
+
+    def getEdges(self,node):
+        return self.nodes[node]
+    
+    # Find heuristic value from node to solution
+    def hn(self,start,end):
+        return ((end[0]-start[0])**2 + (end[1]-start[1])**2)**0.5
+
+    # Find minimum heuristic value from node's edges to solution
+    def minhn(self,node):
+        low = [(100000,100000),100000]
+        edges = self.getEdges(node)
+        for edge in edges:
+            dist = self.hn(edge,self.solution)
+            print(edge,dist)
+            if dist < low[1]:
+                low = [edge,dist]
+        return low
+
 
 def create_map_dictionary(filename):
     mdict = {}
@@ -25,6 +44,9 @@ def create_map_dictionary(filename):
     return mdict
 
 if __name__ == "__main__":
-    g = Graph(create_map_dictionary("map.csv"))
-    print(len(g.nodes))
+    g = Graph((810,10256),create_map_dictionary("map.csv"))
+    n = (1384,5051)
+    minimum = g.minhn(n)
+    print(minimum)
+
     
